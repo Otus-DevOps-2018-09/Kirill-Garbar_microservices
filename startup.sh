2f833f1d7f66bbb1e5e2e93968f1e6b48623ebb5
Sergey Korneev <korneev@tn.ru>
2018-11-29 21:50:08 +0400

    Fixed mongodb install script

--- startup.sh
+++ startup.sh
@@ -19,8 +19,8 @@ function install_mongo {
     echo "Install MongoDB"
 
     # install
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
     apt update
     apt install -y mongodb-org
 
